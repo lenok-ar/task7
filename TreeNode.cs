364ec01691db1a6ef8b08fc59cbe80c96f03ff65
lenok-ar <leno4ka279a@gmail.com>
2025-04-18 23:30:38 +0700

    init TreeNode.cs

---
+++ TreeNode.cs
@@ -0,0 +1,23 @@
+﻿namespace Task7
+{
+    public class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+        public TreeNode<T> Parent { get; set; }
+
+        public TreeNode(T data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+            Parent = null;
+        }
+
+        public override string ToString()
+        {
+            return Data?.ToString() ?? "(null)";
+        }
+    }
+}
